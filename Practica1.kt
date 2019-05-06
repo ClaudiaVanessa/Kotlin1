5447b5158a68f032ab3079f5db996aea19fc20e0
ClaudiaVanessa <claudia.mcueva02@gmail.com>
2019-05-06 09:23:03 -0700

    Practicas en Kotlin

---
+++ Practica1.kt
@@ -0,0 +1,72 @@
+fun main(parametro: Array<String>) {
+     print("Numeros de fibonacci: ")
+val n, f = 0
+val t1 = 1
+val t2 
+
+    print("Numeros que deseas generar de fibonacci: ")
+n = readLine()?.toInt() as Int   
+for (i=1 i=n i++) {
+    t2 = f 
+f = t1 + f
+t1 = t2
+print(t1)
+}
+    }
+
+
+//Examen de PROLOG
+fun main(args: Array<String>) {
+print("Ingrese un numero:")
+     numero = readLine()?.toInt() as Int
+
+print("Ingrese un numero:")
+     numero2 = readLine()?.toInt() as Int     
+
+    println(numero * numero2)
+     print("Multiplicacion: ") 
+}
+
+package ingresando.datos
+    fun main(arg: Array<String>) {
+        
+    print("Ingrese un nombre: ")
+    val nom = readLine()!!
+    println(nom)
+    }
+
+package ingresando.datos
+    fun main(arg: Array<String>) {
+        var num: Int?
+        print("Ingrese un numero: ")
+        num = readLine()?.toInt()
+        println(num)
+    }
+
+package ingresando.datos
+fun main(parametro: Array<String>) {
+     val numero: Int
+    
+    print("Ingrese un num:")
+     num1 = readLine()?.toInt() as Int
+
+    if (numero%2==0){
+        println("El numero es par")  }
+    else{
+        println("El numero es impar")
+}
+    }
+
+fun main(parametro: Array<String>) {
+
+    print("Ingrese primer valor: ")
+    val valor1 = readLine()!!.toInt()
+
+    print("Ingrese segundo valor: ")
+    val valor2 = readLine()!!.toInt()
+
+    if (valor1 > valor2)
+        println("El mayor valor es $valor1")
+    else
+        println("El mayor valor es $valor2")
+}
---
+++ mult.kt
@@ -0,0 +1,22 @@
+fun main(args: Array<String>) {
+    var a = 10 //creamos y asignamos valores 
+    var b = 5
+
+    print("Multiplicacion: ") 
+    println(a * b)
+}
+
+//
+fun main(args: Array<String>) {
+    
+print("Ingrese un numero:")
+    val numero = readLine()?.toInt() as Int
+
+print("Ingrese un numero:")
+    val numero2 = readLine()?.toInt() as Int     
+
+    val res = numero * numero2
+     print("Multiplicacion: $res ") 
+} 
+
+//fun mult(a:Double, b:Double):Double = a * b
